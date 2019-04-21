0cbfc89a9857dd8a18a7f78a5d7de1c492795bf9
Lucas A. Soares <lucassoares1793@gmail.com>
2019-04-21 17:35:15 -0300

    basic template for click scripting (works for storing and selling items

---
+++ right_click.py
@@ -0,0 +1,20 @@
+import time
+from DropsBot.Mouse import Mouse
+from DropsBot.utils import getRagWindow, focusWindow
+
+time.sleep(1)
+
+w = getRagWindow()
+m = Mouse()
+m.move_mouse((2070, 200))
+# m.move_mouse((2270, 400))
+focusWindow(w)
+# m._do_event(m._get_button_value("left", False), 0, 0, 0, 0)
+
+for _ in range(120):
+	print("Clicking down")
+	m._do_event(m._get_button_value("right", False), 0, 0, 0, 0)
+	time.sleep(0.1)
+	print("Clicking up")
+	m._do_event(m._get_button_value("right", True), 0, 0, 0, 0)
+	time.sleep(0.4)
