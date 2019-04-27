40093596518b27403de4434b92f341682748b093
Lucas A. Soares <lucassoares1793@gmail.com>
2019-04-27 12:44:25 -0300

    Add extract channel function for opencv images

--- DropsBot/utils.py
+++ DropsBot/utils.py
@@ -1,3 +1,4 @@
+import os
 import pyautogui as gui
 import pygetwindow
 
@@ -15,3 +16,12 @@ def focusWindow(w):
 	if w.isMinimized:
 		w.restore()
 	w.activate()
+
+def extractChannel(img, channel):
+	if os.sys.platform == "win32":
+		channels = { "red": 2, "green": 1, "blue": 0 } # Windows is BGR
+	else:
+		channels = { "red": 0, "green": 1, "blue": 1 }
+
+	return img[:, :, channels[channel]]
+	
