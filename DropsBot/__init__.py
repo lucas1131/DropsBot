2633cb79ad5d6ee4a8e2c15c9247ed09ceaba1ba
Lucas A. Soares <lucassoares1793@gmail.com>
2019-04-30 18:20:54 -0300

    Remove main and add module's __init__

---
+++ DropsBot/__init__.py
@@ -0,0 +1,9 @@
+from .virtual_hardware import KeyCodes
+from .virtual_hardware import Keyboard
+from .virtual_hardware import Mouse
+
+from .utils import microsleep
+from .utils import getRagWindow
+
+from .game_loop import start
+
